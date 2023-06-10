8883d107173a1aaff658417a0760e299a1347ff7
Codingwithcn <67172682+codingwithcn@users.noreply.github.com>
2023-06-10 17:34:29 -0400

    users can login with email/username and password

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -44,4 +44,23 @@ export class UserResolver {
             );
         }
     }
+
+    @Mutation( returns => String ) 
+    async UserLogIn( @Arg("username") username: string, @Arg("password") password: string ) {
+        let user = await models.Users.findOne({ where: { username } });
+
+        if ( !user ) user = await models.Users.findOne({ where: { email: username }}) // Just incase they are using there email
+
+        if ( user ) {
+            if ( await bcrypt.compare(password, user.password) ) {
+                return jwt.sign(
+                    await Utils.generateJsWebToken(user.id),
+                    Utils.SECRET_KEY,
+                    { expiresIn: "2w" }
+                );
+            }
+        }
+
+        throw new Utils.CustomError("Invalid credentials. Please try again")
+    }
 }
