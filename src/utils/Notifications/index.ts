2835f6835c8619bfba1f40b635217c2287650230
Swiftchris20 <christian.nwobu0@gmail.com>
2024-07-20 21:44:47 -0400

    commit

--- src/utils/Notifications/index.ts
+++ src/utils/Notifications/index.ts
@@ -1,7 +1,52 @@
+import addNotification from 'react-push-notification';
+import {Notifications} from 'react-push-notification';
+
+export function Notification(){
+  function warningNotification() {
+    addNotification({
+      title: "Warning",
+      subtitle: "",
+      message:"",
+      theme:"red",
+      closeButton:"X"
+    });
+  }
+
+  function successNotification() {
+    addNotification({
+        title: "Success",
+        subtitle: "",
+        message: "",
+        theme: "light",
+        closeButton: "X",
+        backgroundTop: "green",
+        backgroundBottom: "yellowgreen",
+    });
+
+    function NewFollowerNotification(userId: string) {
+      addNotification({
+          title: "NewFollower",
+          subtitle: "You have a new follower",
+          message: `${userId} has followed you`,
+          theme: "light",
+          closeButton: "X",
+          backgroundTop: "green",
+          backgroundBottom: "yellowgreen",
+      });
+  }
+
+    if(!Notification){
+      warningNotification();
+    }
+    else{
+      successNotification();
+    }
+  }
+}
+  export default Notification;
 
 
-
-//export const Notifications = async ({message: string, type: string }) => {
+/** //export const Notifications = async ({message: string, type: string }) => {
    // return (
        
             Notification.requestPermission().then(perm => {
@@ -22,3 +67,4 @@
 //};
 
 export  {Notifications, Notification};
+**/
