a10680b0311da1f99ae7d20e59c81b072a3e5582
Codingwithcn <codingwithcn@gmail.com>
2024-08-17 19:05:58 -0400

    saving

--- src/resolvers/EventResolver.ts
+++ src/resolvers/EventResolver.ts
@@ -526,6 +526,23 @@ export class EventResolver {
         }
     }
 
+    @Query( () => [ models.EventTicketReview ])
+    async getEventTicketsReview( @Arg('event_id') event_id: string ) {
+        let event = await models.Events.findOne({ where: { id: event_id } });
+
+        if ( !event ) return new Utils.CustomError("Event not found");
+
+        let eventTicketCarts = await models.EventTicketCart.find({ where: { eventId: event_id, reviewCompleted: true, completed: true }, relations: [ 'review' ] });
+
+        return eventTicketCarts.map( carts => ({
+            photo: carts.review.photo || event.banner;
+            name: string;
+            rating: number;
+            description: string;
+            dateCompleted: Date | null;
+        }) )
+    }
+
     @Query( () => models.EventTicketReview )
     async getTicketReview( @Arg('payment_intent') payment_intent: string ) {
 
