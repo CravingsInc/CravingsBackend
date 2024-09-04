fae2a990d29bbc91c2a2d20af854be9b37fa0084
Codingwithcn <codingwithcn@gmail.com>
2024-09-04 12:03:54 -0400

    saving

--- src/models/types/ContactInput.ts
+++ src/models/types/ContactInput.ts
@@ -20,3 +20,18 @@ export class ContactInput {
     @Field({ nullable: true })
     organizer?: boolean;
 }
+
+@InputType() 
+export class WaitListInput {
+    @Field()
+    first_name: string;
+
+    @Field()
+    last_name: string;
+
+    @Field()
+    email: string;
+
+    @Field()
+    phone_number: string;
+}
