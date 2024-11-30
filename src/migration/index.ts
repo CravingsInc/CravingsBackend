eca1f09c4023452bc65b51377d4314d1eaea2842
Codingwithcn <67172682+codingwithcn@users.noreply.github.com>
2024-11-30 14:43:36 -0500

    saving dev almost done

--- src/migration/index.ts
+++ src/migration/index.ts
@@ -1,11 +1,19 @@
+//#region Imports
+
+// ENV variables
 import "reflect-metadata";
 require('dotenv').config()
 
-import * as models from '../models';
-import { Utils } from '../utils';
-import migrationData from './migration.json';
+// Utils
+import { stripeHandler, Utils } from '../utils';
 
+// Database
 import { createConnection } from "typeorm";
+import migrationData from './migration.json';
+import * as models from '../models';
+
+// Stripe
+import Stripe from "stripe";
 import { stripe } from "../utils/stripe";
 
 //#region Clear Database
@@ -70,8 +78,8 @@ const userRandomFollow = async ( userIds: string[], totalFollowers: number = 3 )
 
 //#region Organizers
 
-const createOrganizer = async (): Promise<string[]> => {
-    let orgIds: string[] = [];
+const createOrganizer = async (): Promise<{ id: string, stripeAccount: string }[]> => {
+    let orgIds: { id: string, stripeAccount: string }[] = [];
 
     for ( let testOrganizer of migrationData.organizers ) {
         let organizer = await models.Organizers.create({
@@ -86,7 +94,7 @@ const createOrganizer = async (): Promise<string[]> => {
             location: testOrganizer.location
         }).save();
 
-        orgIds.push( organizer.id );
+        orgIds.push({ id: organizer.id, stripeAccount: testOrganizer.stripeConnectId });
     }
 
     return orgIds;
@@ -94,23 +102,110 @@ const createOrganizer = async (): Promise<string[]> => {
 
 //#region Events
 
-const createOrganizerEvents = async ( orgIds: string[], totalEvent: number = 4 ) => {
+const createOrganizerEvents = async ( orgs: { id: string, stripeAccount: string }[], totalEvent: number = 4 ) => {
     let orgsEventsIds: {
-        [ x: string ]: string;
+        [ x: string ]: string[];
     } = {};
 
-    for ( let orgId of orgIds ) {
+    for ( let org of orgs ) {
 
-        let randomDate = Date.now() + Math.floor( Math.random() * 1e10 ); // Future timestamp
+        const randomDate = Date.now() + Math.floor( Math.random() * 1e10 ); // Future timestamp
         const endDate = randomDate + Math.floor( Math.random() * 1e6 ); // End after a random date;
 
         for ( let i = 0; i < totalEvent; i++ ) {
             let event = migrationData.events[ Math.floor( Math.random() * migrationData.events.length ) ];
             
-            const existingProduct = stripe.products.list({
-                
-                metadata: { eventIdentifier: `${orgId}-${event.title.replaceAll(" ", "-")}`} // allows for no duplicate of product just to keep stripe test clean as possible
-            })
+            let existingProducts: { data: Stripe.Product[] };
+
+            try {
+                existingProducts = await stripe.products.list({
+                    stripeAccount: org.stripeAccount
+                });
+            }catch( e ) {
+                existingProducts = { data: [] };
+            }
+            
+            // allows for no duplicate of product just to keep stripe test clean as possible
+            const existingProduct = existingProducts.data.find(
+                product => product.metadata?.eventIdentifier === `${org.id}-${event.title.replaceAll(" ", "-")}`
+            );
+
+            let eventSaved = await models.Events.findOneBy({ organizer: { id: org.id }, title: event.title });
+
+            if ( existingProduct ) {
+
+                if ( !eventSaved ) {
+                    eventSaved = await models.Events.create({
+                        title: event.title,
+                        description: event.description.join('\n\n'),
+                        eventDate: new Date(randomDate),
+                        endEventDate: new Date(endDate),
+                        organizer: { id: org.id },
+                        banner: event.banner,
+                        visible: true,
+                        location: event.location,
+                        productId: existingProduct.id
+                    }).save();
+
+                    orgsEventsIds[ org.id ] = orgsEventsIds[ org.id ] || [];
+                    orgsEventsIds[ org.id ].push( eventSaved.id );
+                }
+            }else {
+                if ( !eventSaved ) {
+                    eventSaved = await models.Events.create({
+                        title: event.title,
+                        description: event.description.join('\n\n'),
+                        eventDate: new Date(randomDate),
+                        endEventDate: new Date(endDate),
+                        organizer: { id: org.id },
+                        banner: event.banner,
+                        visible: true,
+                        location: event.location
+                    }).save();
+
+
+                    try {
+                        let stripeProduct = await stripeHandler.createEvent( org.stripeAccount, org.id, eventSaved.id, eventSaved.title, {
+                            eventIdentifier: `${org.id}-${event.title.replaceAll(" ", "-")}`
+                        });
+    
+                        eventSaved.productId = stripeProduct.id;
+                        await eventSaved.save();
+
+                        orgsEventsIds[ org.id ] = orgsEventsIds[ org.id ] || [];
+                        orgsEventsIds[ org.id ].push( eventSaved.id );
+                    }catch( e ) {
+                        await eventSaved.remove(); // want to self clean database
+
+                        eventSaved = null;
+                    }
+                }
+            }
+
+            if ( eventSaved ) {
+
+                // Create event tickets
+                for ( let price of event.tickets ) {
+                    let ticket = await models.EventTickets.create({
+                        title: price.title,
+                        description: price.description,
+                        event: { id: eventSaved.id },
+                        amount: price.amount,
+                        totalTicketAvailable: price.totalAvailable,
+                        currency: 'usd'
+                    }).save();
+
+                    try {
+                        let stripeTicket = await stripeHandler.createEventPrice( org.stripeAccount, org.id, eventSaved.id, eventSaved.productId, ticket.amount, "usd" );
+                        
+                        ticket.priceId = stripeTicket.id;
+            
+                        await ticket.save();
+                    }catch(err) {
+                        await ticket.remove(); // want to self clean database
+                    }
+                }
+            }
         }
     }
 }
